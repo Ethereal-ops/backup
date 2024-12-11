a74e75e5227cd61e0038498c7ee3a343cc11a4f2
Anon Cyber <osamathebomb.er.3@gmail.com>
2024-12-11 19:37:43 +0100

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
   return (
     <section className="relative w-full h-screen bg-[url('/phe7.jpg')] bg-cover bg-center flex items-center justify-center text-white">
       <div className="text-center px-4 flex flex-col items-center gap-4">
-        <h1 className="text-4xl md:text-6xl font-bold">Introducing DappDoctor</h1>
+        <h1 className="text-4xl md:text-6xl font-bold">Introducing ChainPatch</h1>
         <p className="mt-4 text-lg md:text-xl max-w-lg mx-auto">
           Your ultimate solution for troubleshooting wallet issues in the
           blockchain world.
